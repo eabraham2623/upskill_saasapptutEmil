6f9f6fa4189012cd9c90d7fb4eda29589bb53a6c
Emil Abraham <eabraham2623@gmail.com>
2017-06-14 20:27:21 +0000

    Updated flash message styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   
   before_action :configure_premitted_parameters, if: :devise_controller?
   
-  
+  # Whitelist the following form fields
   protected
     def configure_premitted_parameters
         devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:stripe_card_token, :email, :password, :password_confirmation) }
